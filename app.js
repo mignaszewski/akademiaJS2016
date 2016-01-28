36ccab9c4101d2d2a5a40628650aa3e503f49795
ponyhunt3r <annaczech93@gmail.com>
2016-01-28 10:05:59 +0100

    Searching working for a limited amount of sites (10 in this case)

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
-var aom = []; //array of modules
-angular.module("Store", []).
-controller('StoreController', ['$http', '$scope', function($http, $scope) {
+var Store = angular.module("Store", ['ui.bootstrap']);
+
+Store.controller('StoreController', ['$http', '$scope', function($http, $scope) {
     $scope.query ="";
-
-
-    $scope.URL = "https://www.googleapis.com/customsearch/v1";
-
+    $scope.myURL = "https://www.googleapis.com/customsearch/v1";
 
     $scope.queryObj = {
         key: "AIzaSyAfv88ge0srbdRAMM92WpZfYhSFc0IDcRY",
@@ -19,12 +16,11 @@ controller('StoreController', ['$http', '$scope', function($http, $scope) {
     $scope.queryObj.startIndex = 1;
     $scope.resultCount = 0;
 
-    $scope.pagesCount = function(resultNum){
+    $scope.pagesCount = function(resultNum){  //function that return total number of pages to display
         return Math.floor(resultNum / 10);
     }
+    $scope.pagesNumber = 0;
 
-
-    $scope.currentPage = 1;
 
 
 
@@ -33,8 +29,6 @@ controller('StoreController', ['$http', '$scope', function($http, $scope) {
     $scope.items = 3;
     $scope.current = 'second.jpg';
     $scope.myArr = [1, 2, 3];
-
-
 
     $scope.toggle = function(newPic){
     $scope.current =newPic;
@@ -74,80 +68,115 @@ controller('StoreController', ['$http', '$scope', function($http, $scope) {
     }
 
     $scope.haveResults = false;
+    $scope.pagesNumArr = [];
 
 
-    $scope.getRequest = function() {
+    //$scope.getRequest = function() {
+    //    $scope.queryObj.q = $scope.query;
+    //    $http({
+    //        method: 'GET',
+    //        url: $scope.URL,
+    //        params: $scope.queryObj
+    //    }).then(function successCallback(response) {
+    //        $scope.searchResult = response.data.items;
+    //        console.log(response.data.items);
+    //        $scope.haveResults = true;
+    //        $scope.pagesNumber = pagesCount(response.data.queries.totalResults);
+    //        console.log(pagesNumber);
+    //        for (var i = 0; i < pagesNumber; i++)
+    //        {$scope.pagesNumArr.push(i);
+    //        }
+    //        $scope.nextQuery = response.data.queries.nextPage;
+    //    }, function errorCallback(response) {
+    //        $scope.searchResult = response.data.error();
+    //    });
+    //};
 
-        $scope.queryObj.q = $scope.query;
 
-        $http({
-            method: 'GET',
-            url: $scope.URL,
-            params: $scope.queryObj
-        }).then(function successCallback(response) {
-            $scope.searchResult = response.data.items;
-            console.log(response.data.items);
+    //$scope.callNext = function(pgnumber){
+    //
+    //    pgnumber *= 10;
+    //    var myurl = 'https://www.googleapis.com/customsearch/v1?' +
+    //    'googlehost=google.co.uk&safe=medium&searchType=image&num=8&start='+pgnumber+'&' +
+    //    'key=AIzaSyCwA35ibvlnqrIbjGQepRtBVWJNHB3hMdA&cx=010146182105979843094:z2dcm8ayrqu&q=';
+    //    $http.get(myurl + $scope.query).success(function(data){
+    //        $scope.searchResult = response.data.items;
+    //        // $http.get('scripts/json/temporary-google-data.json').success(function(data){
+    //        // 	$scope.results = data.items;
+    //    })
+    //};
+    //
+    //$scope.callPrevious= function(){
+    //
+    //    $scope.queryObj.start-= 10;
+    //    $scope.
+    //
+    //    $http({
+    //        method: 'GET',
+    //        url: $scope.URL,
+    //        params: $scope.queryObj
+    //    }).then(function successCallback(response) {
+    //        $scope.searchResult = response.data.items;
+    //        console.log(response.data.items);
+    //        $scope.haveResults = true;
+    //
+    //
+    //
+    //    }, function errorCallback(response) {
+    //        //$scope.searchResult = response.data.error();
+    //    });
+    //
+    //
+    //
+    //}
+
+    //$scope.filteredTodos = []
+    //    ,$scope.currentPage = 1
+    //    ,$scope.numPerPage = 10
+    //    ,$scope.maxSize = 5;
+    //
+    //$scope.makeTodos = function() {
+    //    $scope.todos = [];
+    //    for (i=1;i<=1000;i++) {
+    //        $scope.todos.push({ text:'todo '+i, done:false});
+    //    }
+    //};
+    //$scope.makeTodos();
+    //
+    //$scope.numPages = function () {
+    //    return Math.ceil($scope.todos.length / $scope.numPerPage);
+    //};
+    //
+    //$scope.$watch('currentPage + numPerPage', function() {
+    //    var begin = (($scope.currentPage - 1) * $scope.numPerPage)
+    //        , end = begin + $scope.numPerPage;
+    //
+    //    $scope.filteredTodos = $scope.todos.slice(begin, end);
+    //});
+
+    $scope.pgnumbers = [1, 9, 17, 25, 33, 41, 49, 57, 65, 73];
+    $scope.pageNumbers = [];
+    $scope.totalResults=0;
+    $scope.results = [];
+
+    $scope.getResults = function(pgnumber){
+        var url = 'https://www.googleapis.com/customsearch/v1?' +
+            'googlehost=google.co.uk&safe=medium&searchType=image&num=8&start='+pgnumber+'&' +
+            'key=AIzaSyAfv88ge0srbdRAMM92WpZfYhSFc0IDcRY&cx=011106400896487923477:vutdejdv5t0&q=';
+        $http.get(url + $scope.query + "'").success(function(data){
+            $scope.results = data.items;
             $scope.haveResults = true;
-
-            $scope.nextQuery = response.data.queries.nextPage;
-
-
-
-
-        }, function errorCallback(response) {
-            $scope.searchResult = response.data.error();
-        });
+            //$scope.totalResults = data.searchInformation.totalResults;
+            //
+            //for (var i = 0; i < $scope.totalResults; i++)
+            //{
+            //    $scope.pageNumbers.push(i);
+            //}
 
 
 
+        })
     };
-
-
-    $scope.callNext = function() {
-
-        $scope.queryObj.start += 10;
-
-            $http({
-            method: 'GET',
-            url: $scope.URL,
-            params: $scope.queryObj
-        }).then(function successCallback(response) {
-            $scope.searchResult = response.data.items;
-            console.log(response.data.items);
-            $scope.haveResults = true;
-
-
-
-        }, function errorCallback(response) {
-            //$scope.searchResult = response.data.error();
-        });
-
-
-    }
-
-    $scope.callPrevious= function(){
-
-        $scope.queryObj.start-= 10;
-        $scope.
-
-        $http({
-            method: 'GET',
-            url: $scope.URL,
-            params: $scope.queryObj
-        }).then(function successCallback(response) {
-            $scope.searchResult = response.data.items;
-            console.log(response.data.items);
-            $scope.haveResults = true;
-
-
-
-        }, function errorCallback(response) {
-            //$scope.searchResult = response.data.error();
-        });
-
-
-
-    }
 
 
 
@@ -181,7 +210,31 @@ controller('StoreController', ['$http', '$scope', function($http, $scope) {
         }
     ];
 
-
-
 }]);
 
+
+Store.controller('TodoController', function($scope) {
+    $scope.filteredTodos = []
+        ,$scope.currentPage = 1
+        ,$scope.numPerPage = 10
+        ,$scope.maxSize = 5;
+
+    $scope.makeTodos = function() {
+        $scope.todos = [];
+        for (i=1;i<=1000;i++) {
+            $scope.todos.push({ text:'todo '+i, done:false});
+        }
+    };
+    $scope.makeTodos();
+
+    $scope.numPages = function () {
+        return Math.ceil($scope.todos.length / $scope.numPerPage);
+    };
+
+    $scope.$watch('currentPage + numPerPage', function() {
+        var begin = (($scope.currentPage - 1) * $scope.numPerPage)
+            , end = begin + $scope.numPerPage;
+
+        $scope.filteredTodos = $scope.todos.slice(begin, end);
+    });
+});
