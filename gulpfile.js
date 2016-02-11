7c7f92c4427916759ab1788974e2a8ff9d427698
ponyhunt3r <annaczech93@gmail.com>
2016-02-11 15:01:03 +0100

    First working build using bower and gulp, js file changed with ng-annotation, work with comand gulp (pre build on port 8888) and gulp build (final release designed to be pushed to server)

--- gulpfile.js
+++ gulpfile.js
@@ -1,143 +1,79 @@
+
+
 // gulp
 var gulp = require('gulp');
 
 // plugins
 var connect = require('gulp-connect');
-// var jshint = require('gulp-jshint');
+var jshint = require('gulp-jshint');
 var uglify = require('gulp-uglify');
 var minifyCSS = require('gulp-minify-css');
 var clean = require('gulp-clean');
-var browserify = require('gulp-browserify');
-var concat = require('gulp-concat');
 var runSequence = require('run-sequence');
-var less = require('gulp-less');
-var browserSync = require('browser-sync').create();
-var bg = require("gulp-bg");
+var imageop = require('gulp-image-optimization');
 
-var rename = require('gulp-rename');
-var loopbackAngular = require('gulp-loopback-sdk-angular');
-
-//#############################################################################
-var loopback = require('loopback');
-var boot = require('loopback-boot');
-
-gulp.task("api", bg("node", "."));
-
-gulp.task('slc-loopback', function () {
-    return gulp.src('./server/server.js')
-    .pipe(loopbackAngular())
-    .pipe(rename('lb-services.js'))
-    .pipe(gulp.dest('./app/client/js'));
+// tasks
+gulp.task('lint', function() { //checks if js files are all correct
+  gulp.src(['./Shop/**/*.js', '!./Shop/bower_components/**']) 
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .pipe(jshint.reporter('fail'));
 });
-
-// Default task !
-gulp.task('default', function() {
-  runSequence(['clean'], ['serve', 'slc-loopback'], ['api'], function() {
-  });
+gulp.task('clean', function () {  //removes everything from /dist folder
+  return gulp.src('./dist/*', {read: false})
+    .pipe(clean());
 });
-
-// Build task
-gulp.task('build', ['clean'], function() {
-  setTimeout(function() {
-    runSequence(['clean'], ['copy-bower-components', 'copy-html-files', 'minify-css', 'browserifyDist', 'connectDist'], function () {
-
-    });
-  },2000);
-});
-
-// clean task
-gulp.task('clean', function() {
-    gulp.src('./dist/*')
-      .pipe(clean({force: true}))
-    gulp.src('./app/js/bundled.js')
-      .pipe(clean({force: true}))
-});
-
-// create a task that ensures the `js` task is complete before reloading browsers, delay by 1sec
-gulp.task('js-watch', ['browserify'], function() {
-  setTimeout(browserSync.reload, 1000);
-});
-
-gulp.task('html-watch', function() {
-  setTimeout(browserSync.reload, 1000);
-});
-
-
-// Refreshing page after change, starting browser for dev
-gulp.task('serve', ['browserify', 'compile-less'], function() {
-  browserSync.init({
-    server: "./app"
-  });
-  gulp.watch(['./app/js/**/*.js', '!./app/bower_components/**', '!./app/js/bundled.js'], ['js-watch']);
-  gulp.watch('./app/less/**/*.less' , ['compile-less', browserSync.reload]);
-  gulp.watch('./app/**/*.html' , ['html-watch']);
-});
-
-/* Task to compile less */
-gulp.task('compile-less', function() {
-  gulp.src('./app/less/**/*.less')
-    .pipe(less())
-    .pipe(gulp.dest('./app/css/'));
-});
-
-
-// gulp.task('lint', function() {
-//   gulp.src(['./app/**/*.js', '!./app/bower_components/**', '!./app/js/bundled.js'])
-//     .pipe(jshint())
-//     .pipe(jshint.reporter('default'))
-//     .pipe(jshint.reporter('fail'));
-// });
-
-gulp.task('minify-css', function() {
+gulp.task('minify-css', function() {  //minifies css files
   var opts = {comments:true,spare:true};
-  gulp.src(['./app/**/*.css', '!./app/bower_components/**'])
+  gulp.src(['./Shop/**/*.css', '!./Shop/bower_components/**'])
     .pipe(minifyCSS(opts))
-    .pipe(gulp.dest('./dist/'));
+    .pipe(gulp.dest('./dist/'))
 });
-
-gulp.task('minify-js', function() {
-  gulp.src(['./app/**/*.js', '!./app/bower_components/**'])
+gulp.task('minify-js', function() {   //minifies js giles 
+  gulp.src(['./Shop/**/*.js', '!./Shop/bower_components/**'])
     .pipe(uglify({
       // inSourceMap:
       // outSourceMap: "app.js.map"
     }))
-    .pipe(gulp.dest('./dist/'));
+    .pipe(gulp.dest('./dist/'))
 });
-
-gulp.task('copy-bower-components', function () {
-  gulp.src('./app/bower_components/**')
-    .pipe(gulp.dest('dist/bower_components'));
+gulp.task('copy-bower-components', function () {  //copies front end depedencies
+  gulp.src('./Shop/bower_components/**')
+    .pipe(gulp.dest('dist/bower_components/'));
 });
-
-gulp.task('copy-html-files', function () {
-  gulp.src('./app/**/*.html')
+gulp.task('copy-html-files', function () {   // copies html files
+  gulp.src('./Shop/**/*.html')
     .pipe(gulp.dest('dist/'));
 });
-
-
-gulp.task('browserify', function() {
-  gulp.src(['app/js/main.js'])
-  .pipe(browserify({
-    insertGlobals: true,
-    debug: true
-  }))
-  .pipe(concat('bundled.js'))
-  .pipe(gulp.dest('./app/js'))
+gulp.task('connect', function () {  //conects to localhost on port 8888
+  connect.server({
+    root: 'Shop/',
+    port: 8888
+  });
 });
-
-gulp.task('browserifyDist', function() {
-  gulp.src(['app/js/main.js'])
-  .pipe(browserify({
-    insertGlobals: true,
-    debug: true
-  }))
-  .pipe(concat('bundled.js'))
-  .pipe(gulp.dest('./dist/js'))
-});
-
-gulp.task('connectDist', function () {
+gulp.task('connectDist', function () {  //connects to localhost on port 9999
   connect.server({
     root: 'dist/',
     port: 9999
   });
+});
+
+gulp.task('images', function(cb) {   //compresses images
+    gulp.src(['Shop/**/*.png','Shop/**/*.jpg','Shop/**/*.gif','Shop/**/*.jpeg']).pipe(imageop({
+        optimizationLevel: 5,
+        progressive: true,
+        interlaced: true
+    })).pipe(gulp.dest('dist/')).on('end', cb).on('error', cb);
+});
+
+
+// default task
+gulp.task('default',
+  ['lint', 'connect']    //cmd: gulp, connect on localhost
+);
+gulp.task('build', function() { //cmd: gulp build, creates a final build meant to be pushed to server
+  runSequence(
+    ['clean'],
+    ['clean' , 'lint', 'minify-css', 'minify-js', 'copy-html-files', 'copy-bower-components', 'connectDist', 'images']
+  );
 });
